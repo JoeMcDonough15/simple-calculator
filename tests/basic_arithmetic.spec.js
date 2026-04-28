b46470540520fd2dfcda4b2816c147c64dcc9c36
joemcdonough15 <joemcdonoughmusic@gmail.com>
2026-04-28 03:14:57 -0400

    write test for basic subtraction

--- tests/basic_arithmetic.spec.js
+++ tests/basic_arithmetic.spec.js
@@ -5,30 +5,34 @@ test("Add Two Numbers", async ({ page }) => {
   await page.goto("http://localhost:3000");
   await page.pause();
   // Click 2
-  await page.locator('[id="num_2"]').click();
+  await page.locator("[id=num_2]").click();
   await expect(page.locator("[id=display-text]")).toHaveText("2");
   // Click +
-  await page.locator('[id="add"]').click();
+  await page.locator("[id=add]").click();
   // Click 2
-  await page.locator('[id="num_2"]').click();
+  await page.locator("[id=num_2]").click();
   await expect(page.locator("[id=display-text]")).toHaveText("2");
   // Click =
-  await page.locator('[id="equals"]').click();
+  await page.locator("[id=equals]").click();
   await expect(page.locator("[id=display-text]")).toHaveText("4");
+  await page.close();
 });
 
 test("Subtract Two Numbers", async ({ page }) => {
   // * 5 - 4 = 1
   await page.goto("http://localhost:3000");
   await page.pause();
-
-  // Click 5
-
-  // Click -
-
-  // Click 4
-
-  // Click =
-
-  // Assert that the difference is 1
+  // Click 5 // id=num_5
+  await page.locator("[id=num_5]").click();
+  await expect(page.locator("[id=display-text]")).toHaveText("5");
+  // Click - // id=subtract
+  await page.locator("[id=subtract]").click();
+  // Click 4 // id=num_4
+  await page.locator("[id=num_4]").click();
+  await expect(page.locator("[id=display-text]")).toHaveText("4");
+  // Click = // id=equals
+  await page.locator("[id=equals]").click();
+  // Assert that the difference is 1 // id=display-text
+  await expect(page.locator("[id=display-text]")).toHaveText("1");
+  await page.close();
 });
