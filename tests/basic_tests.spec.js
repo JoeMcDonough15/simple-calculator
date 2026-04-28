94dfec065f207d330414971a952b4aa74fc20a9c
joemcdonough15 <joemcdonoughmusic@gmail.com>
2026-04-28 19:31:31 -0400

    rename file basic_tests.spec.js, group arithmetic tests into a block, initialize notes for the entering numbers block, and remove any entering number tests from arithmetic block

---
+++ tests/basic_tests.spec.js
@@ -0,0 +1,81 @@
+import { test, expect } from "@playwright/test";
+
+test.beforeEach(async ({ page }) => {
+  await page.goto("http://localhost:3000");
+});
+
+test.afterEach(async ({ page }) => {
+  await page.close();
+});
+
+// * Entering Numbers
+test.describe("Calculator should be able to accept numbers as input and reflect them accurately in display window", () => {
+  // * 5 should be able to be entered
+  // * 28 should be able to be entered
+  // * 294,345 should be able to be entered with comma in the correct place
+  // * 75.39 should be able to be entered with decimal in the correct place
+  // * 0.. should resolve to just 0. without an extra decimal appearing
+  // * -35 should be able to be entered
+  // * 00006 should be able to be entered and appear as just 6
+});
+
+// * Simple Arithmetic
+test.describe("Calculator should be able to perform arithmetic on two numbers", () => {
+  test("Add Two Numbers", async ({ page }) => {
+    // * 2 + 2 = 4
+    // Press 2
+    await page.locator("[id=num_2]").click();
+    // Press +
+    await page.locator("[id=add]").click();
+    // Press 2
+    await page.locator("[id=num_2]").click();
+    // Press =
+    await page.locator("[id=equals]").click();
+    // Assert that the display text says 4
+    await expect(page.locator("[id=display-text]")).toHaveText("4");
+  });
+
+  test("Subtract Two Numbers", async ({ page }) => {
+    // * 5 - 4 = 1
+    // Press 5
+    await page.locator("[id=num_5]").click();
+    // Press -
+    await page.locator("[id=subtract]").click();
+    // Press 4
+    await page.locator("[id=num_4]").click();
+    // Press =
+    await page.locator("[id=equals]").click();
+    // Assert that the display text says 1
+    await expect(page.locator("[id=display-text]")).toHaveText("1");
+  });
+
+  test("Multiply Two Numbers", async ({ page }) => {
+    // * 9 x 7 = 63
+    // Press 9
+    await page.locator("[id=num_9]").click();
+    // Press multiply
+    await page.locator("[id=multiply]").click();
+    // Press 7
+    await page.locator("[id=num_7]").click();
+    // Press =
+    await page.locator("[id=equals]").click();
+    // Assert that the display text says 63
+    await expect(page.locator("[id=display-text]")).toHaveText("63");
+  });
+
+  test("Divide Two Numbers", async ({ page }) => {
+    // * 28 ÷ 7 = 4
+    // Press 2
+    await page.locator("[id=num_2]").click();
+    // Press 8
+    await page.locator("[id=num_8]").click();
+    // Press divide
+    await page.locator("[id=divide]").click();
+    // Press 7
+    await page.locator("[id=num_7]").click();
+    // Press =
+    await page.locator("[id=equals]").click();
+    // Assert that the display text says 4
+    await expect(page.locator("[id=display-text]")).toHaveText("4");
+  });
+});
